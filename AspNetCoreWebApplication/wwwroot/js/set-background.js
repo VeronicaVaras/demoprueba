229335db4050fac4512d925417578c55ca4d7017
VeronicaVaras <72535020+VeronicaVaras@users.noreply.github.com>
2021-05-04 16:13:40 -0500

    v1

--- AspNetCoreWebApplication/wwwroot/js/set-background.js
+++ AspNetCoreWebApplication/wwwroot/js/set-background.js
@@ -1,3 +1,20 @@
 var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream)=>{
+
+    console.log(stream)
+  
+  
+  
+    let video = document.getElementById('video')
+  
+  
+  
+    video.srcObject = stream
+  
+  
+  
+  
+  }).catch((err)=>console.log(err))
